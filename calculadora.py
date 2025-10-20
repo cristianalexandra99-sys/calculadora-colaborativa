78f5dfc6a40fe2020f7230e0c3994dd2f8dccd35
cristianalexandra99-sys <cristianalexandra99@gmail.com>
2025-10-20 21:25:36 +0100

    Create calculadora.py
    Adiciona funções de soma e subtração

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+#calculadora.py
+# Projeto colaborativo - GitHub Desktop
+# Autor: Cristiana e Tiago
+
+def soma(a,b):
+    """Retorna a soma de dois números. """
+    return a+b
+
+def subtrai(a,b):
+    """Retorna a subtração de dois números."""
+    return a - b
+print("Calculadora colaborativa!")
+print("Soma: ", soma(5, 3))
+print("Substração: ", subtrai(10,4))
